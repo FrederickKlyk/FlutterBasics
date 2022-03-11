8ae128ff74187c5938ff7b33a6cff6bde8d0fb11
Frederick Klyk <klyk@adesso-mobile.de>
2022-03-11 15:10:16 +0100

    main: log statement with future then

--- lib/domain/personen_interactor_impl.dart
+++ lib/domain/personen_interactor_impl.dart
@@ -1,14 +1,24 @@
 import 'package:demo1/domain/personen_interactor.dart';
 import 'package:demo1/io/local/floor/Person.dart';
 import 'package:demo1/io/local/floor/PersonDao.dart';
+import 'package:logger/logger.dart';
 
 class PersonenInteractorImpl extends PersonenInteractor {
   final PersonDao personDao;
+  final logger = Logger();
 
   PersonenInteractorImpl(this.personDao);
 
   @override
   Stream<Person?> findPersonById(int id) {
+    getAllPersonen().then((result) {
+      if (result.isEmpty) {
+        logger.d("Film Liste ist leer");
+      } else {
+        logger.d("Es müsste ein neuer Film rein");
+      }
+    });
+
     return personDao.findPersonById(id);
   }
 
